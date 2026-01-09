22c726cd95f01d18112db86d2c002a808d79dffd
KennySalazar2502 <156482653+KennySalazar2502@users.noreply.github.com>
2026-01-09 01:09:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,29 @@
-const CACHE="cristo-v3";
-const FILES=[
-  "./",
-  "./index.html",
-  "./calendar.html",
-  "./live.html",
-  "./style.css",
-  "./main.js",
-  "./logo.png"
+const CACHE_NAME = "cristo-cache-v2";
+const urlsToCache = [
+  "/index.html",
+  "/calendar.html",
+  "/live.html",
+  "/style.css",
+  "/main.js",
+  "/manifest.json",
+  "/logo.png",
+  "/images/icon-192.png",
+  "/images/icon-512.png",
+  "/images/youtube logo.webp",
+  "/images/facebook logo.jpg"
 ];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
